597d9e2fa2c323d356ea515c57b689adf9eeae97
babot <ababot@me.com>
2018-11-10 23:56:30 +0100

    Table Class added

---
+++ Table.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Babot
+ * Date: 10/11/2018
+ * Time: 23:55
+ */
+
+class Table
+{
+
+}
--- list.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Babot
- * Date: 05/11/2018
- * Time: 13:54
- */
-
-//TODO: Implement list class
